724c42533389cf3cd9c2160c29bc621a58cbee5e
FalseGodGames <garg.apoorv1098@gmail.com>
2022-11-20 16:16:55 +0530

    Reset

--- Home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns=[
-    path("",views.index,name="index.html"),
-    #path("index.html/#",views.index,name="index.html"),
-    path("team.html",views.team,name="team.html"),
-    path("submit",views.register,name="register"),
-]
